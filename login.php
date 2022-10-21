ae91314d628fb39712eb139e26d3922a3f564b38
pattana1902 <pattana192002@gmail.com>
2022-10-22 04:22:19 +0700

    Edited By Pattana

--- login.php
+++ login.php
@@ -22,63 +22,62 @@ if (isset($_SESSION['username']) == "member" && isset($_SESSION['username']) == 
 </head>
 
 <body>
-	
-		<div class="container">
-			<?php
-			if (!isset($_SESSION["error"])) {
-				Header("url=login.php");
-			} else if (($_SESSION["error"]) == "f") {
-				session_destroy();
-				echo "<div class='row'>
+
+	<div class="container">
+		<?php
+		if (!isset($_SESSION["error"])) {
+			Header("url=login.php");
+		} else if (($_SESSION["error"]) == "f") {
+			session_destroy();
+			echo "<div class='row'>
 				<div class='col-md-offset-3 col-md-6'>
 				<center>
 				<div class='alert alert-danger'>ชื่อผู้ใช้งานหรือรหัสผ่านไม่ถูกต้อง</div>
 				</center>
 				</div>
 				</div>";
-			}
-			?>
-			<br>
-			<form action="verify.php" method="post">
+		}
+		?>
+		<br>
+		<form action="verify.php" method="post">
 
-				<div class="container">
-					<center><img src="./images/logo remove-bg.png" width="110%"></center>
-					<div class="row">
-						<div class="col-md-offset-3 col-md-6">
-							<div class="form-group col-md-12">
-								<center>
-									<div class="input-icons">
-										<i class="fa fa-user icon">
-										</i>
-										<input class="input-field" type="text" name="Name" placeholder="Username">
-										<center>
-									</div>
-							</div>
-							<div class="form-group col-md-12">
-								<center>
-									<div class="input-icons">
-										<i class="fa fa-key icon">
-										</i>
-										<input class="input-field" type="password" name="Password" placeholder="Password">
-								</center>
-							</div>
+			<div class="container">
+				<center><img src="./images/logo remove-bg.png" width="110%"></center>
+				<div class="row">
+					<div class="col-md-offset-3 col-md-6">
+						<div class="form-group col-md-12">
+							<center>
+								<div class="input-icons">
+									<i class="fa fa-user icon">
+									</i>
+									<input class="input-field" type="text" name="Name" placeholder="Username">
+									<center>
+								</div>
+						</div>
+						<div class="form-group col-md-12">
+							<center>
+								<div class="input-icons">
+									<i class="fa fa-key icon">
+									</i>
+									<input class="input-field" type="password" name="Password" placeholder="Password">
+									<br>
+										<button type="submit" class="button">Login</button>
 							</center>
 						</div>
-
-						<br>
-						<center>
-							<button type="submit" class="button">Login</button>
 						</center>
-						<br>
 					</div>
-					<center>
-						<p>ถ้ายังไม่ได้เป็นสมาชิก <a href="register.php">กรุณาสมัคร</a></p>
-					</center>
-				</div>
 
-			</form>
-		</div>
-	
+
+					<br>
+				</div>
+				<center>
+					<p>ถ้ายังไม่ได้เป็นสมาชิก <a href="register.php">กรุณาสมัคร</a></p>
+				</center>
+			</div>
+
+		</form>
+	</div>
+
 </body>
 
 </html>
